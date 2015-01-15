96e5d12ca0c5ab2b54f4b6f785cca09e4d69b50c
Neil B <batavia.neil@gmail.com>
2015-01-16 00:30:13 +0530

    Posts scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150115010544) do
+ActiveRecord::Schema.define(version: 20150115143613) do
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.string   "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
