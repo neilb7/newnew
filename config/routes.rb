0509ba459833bf1dd9f9691f03e354f291781505
Neil B <batavia.neil@gmail.com>
2015-02-04 23:25:33 +0530

    reset the home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :posts
 
+  root 'posts#index'
+
   devise_for :users
-  root 'pages#home'
 
   get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
